4441d27fc6b18576e18bcafb79d64cf73e23362f
duke <dukelu999@gmail.com>
2025-06-20 21:45:47 +0700

    set_work.py

--- set_work.py
+++ set_work.py
@@ -1,18 +1,24 @@
 set_a = {6,"11",True,49.56,"hello"}
 print(set_a)
+
 lenth = len(set_a)
 print(lenth)
+
 for site in set_a:
 	print(site)     
 if 1000 in set_a:
 	print(Ture)
 else:
 	print(False)
+
 set_a.remove(6)
 print(set_a)
+
 set_a.add(16)
 print(set_a)
+
 set_a.add (16)
 print(set_a)
+
 set_a.clear()
 print(set_a)
