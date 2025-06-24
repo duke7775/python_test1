1a5e41fac5ca65c0db074891150f998f727120bc
duke <dukelu999@gmail.com>
2025-06-24 21:05:42 +0700

    file.py

--- file.py
+++ file.py
@@ -5,10 +5,16 @@ import os
 file = open('info.txt','w')
 
 file.write("hello\n")
+file.tell()
+print(file.tell())
 file.write("good")
+file.tell()
+print(file.tell())
 
 file = open('info.txt','r')
+file.seek(0)
+print(file.tell())
 print(file.read())
- 
+print(file.tell())
 #删除文件
 os.remove('info.txt')
