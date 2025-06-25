b82d84e593c2e89322258ca15010583cd9ff1c6d
duke <dukelu999@gmail.com>
2025-06-25 22:04:38 +0700

    my_json.py

--- my_json.py
+++ my_json.py
@@ -1,4 +1,5 @@
-#说明 json文件
+#说明 
+#功能：测试json文件
 #引入模块
 import json
 import os
@@ -6,12 +7,13 @@ import os
 #定义数据
 data = {"language":"Python","os":"Windows","keywords":["del","print"]}
 
+filepath = 'data.json'
 #写入
-with open("data.json", "w+") as f:
+with open(filepath, "w+") as f:
     json.dump(data,f)
     f.seek(0)
     content = json.load(f)
     print(content)
 
 #删除文件
-os.remove("data.json")
+os.remove(filepath)
