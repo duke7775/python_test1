4e43673b1ea5b3d5e9ddf8057140c8db2f00d7c5
duke <dukelu999@gmail.com>
2025-06-25 21:29:23 +0700

    file,py

--- file.py
+++ file.py
@@ -2,7 +2,9 @@
 #引入os
 import os
 
-file = open('info.txt','w')
+filepath ='info.txt'
+
+file = open(filepath,'w+')
 print(file.tell())
 
 file.write("hello\n")
@@ -11,11 +13,10 @@ print(file.tell())
 file.write("good")
 print(file.tell())
 
-file = open('info.txt','r')
 file.seek(0)
 print(file.tell())
 
 print(file.read())
 print(file.tell())
 #删除文件
-os.remove('info.txt')
+os.remove(filepath)
