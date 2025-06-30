40d5526cf26e003efb40ba7672b7897115edc5b0
duke <dukelu999@gmail.com>
2025-06-30 21:00:30 +0700

    tcp1.py

--- tcp1.py
+++ tcp1.py
@@ -4,8 +4,14 @@ import socket
 
 #创建一个socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('www.sina.com.cn', 80))
+try:
+    s.connect(('www.sina.com.cn', 81))
+except socket.error: 
+    print(f"Connection failed: {e}")
+finally:
+    s.close()
 
+ 
 #发送数据
 s.send(b'GET / HTTP/1.1\r\nHost: www.sina.com.cn\r\nConnection: close\r\n\r\n')
 
