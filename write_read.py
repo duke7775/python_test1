047446c740f44aaceb8e5c64ae7b33fbaf6258f5
duke <dukelu999@gmail.com>
2025-07-12 21:23:43 +0700

    write_read.py

--- write_read.py
+++ write_read.py
@@ -4,12 +4,12 @@ import random
 
 def read_random(file_path):
     result = {}
-    with open(file_path) as file:
+    with open(file_path, encoding='utf-8') as file:
         file.seek(0)
         file_content = file.read()
         max_pos = min(1000,len(file_content) - 1)
         for _ in range(10):
-            pos = random.randint(1, max_pos)
+            pos = random.randint(0, max_pos)
             result[pos] = file_content[pos]
     return result
 
@@ -17,7 +17,7 @@ def read_random(file_path):
  
 def read_row(file_path):
     result = {}
-    with open(file_path) as file:
+    with open(file_path, encoding='utf-8') as file:
         file.seek(0)
         file_content = file.readlines()
         for index, line in enumerate(file_content):
@@ -27,7 +27,13 @@ def read_row(file_path):
 
 def write_random(file_path):
     result = {}
-    with open(file_path, 'w') as file:
+    with open(file_path, 'r+', encoding='utf-8') as file:
+        file_content = file.read()
+        max_pos = min(1000, len(file_content) - 1)
+        if len(file_content) < 1:
+            print("文件为空")
+            return result
+        
         for _ in range(10):
             pos = random.randint(1, 1000)
             char = random.choice('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
@@ -38,7 +44,11 @@ def write_random(file_path):
 
 
 
-file_path = "D:\python\content.txt"
-print(read_random(file_path))
-print(read_row(file_path))
-print(write_random(file_path))
+def process_file(file_path):
+    print("Random Read:", read_random(file_path))
+    print("Row Read:", read_row(file_path))
+    print("Write Random:", write_random(file_path))
+ 
+if __name__ == "__main__":
+    file_path = r"D:\python\content.txt"
+    process_file(file_path)
