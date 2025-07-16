327248061310d8d720193bd34170ddffa0c06e1b
duke <dukelu999@gmail.com>
2025-07-16 21:38:31 +0700

    markdown2html.py

--- markdown2html.py
+++ markdown2html.py
@@ -1,14 +1,16 @@
 #markdown转HTML
 import markdown
 
-with open('markdown2html.md', 'r', encoding='utf-8') as md_file:
-    markdown_text = md_file.read()
+def convert_markdown_to_html() -> None:
+    with open('markdown2html.md', 'r', encoding='utf-8') as md_file:
+        markdown_text = md_file.read()
+    html_output = markdown.markdown(markdown_text)
+    with open('markdown2html.html', 'w', encoding='utf-8') as html_file:
+        html_file.write(html_output)
+    print("Markdown has been converted to HTML and saved as 'markdown2html.html'.")
 
-html_output = markdown.markdown(markdown_text)
+if __name__ == "__main__":
+    convert_markdown_to_html() 
 
-with open('markdown2html.html', 'w', encoding='utf-8') as html_file:
-    html_file.write(html_output)
-
-print("Markdown has been converted to HTML and saved as 'markdown2html.html'.")
 
 
