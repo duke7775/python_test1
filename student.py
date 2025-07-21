0677766b8e6590ed2161dbbca16ccafe70a1775a
duke <dukelu999@gmail.com>
2025-07-21 21:44:08 +0700

    student.py

---
+++ student.py
@@ -0,0 +1,45 @@
+#说明：创建class Student
+import datetime
+
+class Student:
+    name =  str("Duke")
+    age = int(19)
+    nationality = str("China")
+    gender = str("M")
+    math_score = int(90)
+    english_score = int(85)
+
+    def __init__(self, name, age, nationality, gender, math_score, english_score):
+        print("调用\"__init__\"方法")
+        self.name = name
+        self.age = age
+        self.nationality = nationality
+        self.gender = gender
+        self.math_score = math_score
+        self.english_score = english_score
+        self.creation_time = datetime.datetime.now()
+        
+
+    def create(self) -> None:
+        print("调用\"create\"方法")
+        print( f"Student {self.name} created with time {self.creation_time}")
+    
+
+    def list(self) -> None:
+        print("调用\"list\"方法")
+        print( f"Name: {self.name}, Age: {self.age}, Nationality: {self.nationality}, Gender: {self.gender}, Math Score: {self.math_score}, English Score: {self.english_score}, Creation Time: {self.creation_time}")
+    
+
+    def __del__(self):
+        print("调用\"__del__\"方法")
+        print(f"Student {self.name} deleted at {datetime.datetime.now()}")
+        
+
+def main() -> None:
+    student = Student(name="Duke", age=19, nationality="China", gender="M", math_score=90, english_score=85)
+    student.create()
+    student.list()
+    del student
+
+if __name__ == "__main__":
+    main()
